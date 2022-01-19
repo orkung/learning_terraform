1458c76b06e15485ef682f5a51b2e369e1391f89
cavit <cavit@orkun.space>
2022-01-20 01:49:30 +0300

    input output variables added

--- main.tf
+++ main.tf
@@ -1,38 +1,40 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 resource "aws_instance" "example" {
-  ami           = "ami-04505e74c0741db8d"
-  instance_type = "t2.micro"
+  ami                    = "ami-04505e74c0741db8d"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, world" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
 }
 
-#data "aws_ami" "ubuntu" {
-#  most_recent = true
-#
-#  filter {
-#    name   = "name"
-#    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#  }
-#
-#  filter {
-#    name   = "virtualization-type"
-#    values = ["hvm"]
-#  }
-#
-#  owners = ["099720109477"] # Canonical
-#}
-#
-#resource "aws_instance" "web" {
-#  ami           = data.aws_ami.ubuntu.id
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "HelloWorld"
-#  }
-#}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
 
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  type        = number
+  description = "The port the server will use for HTTP Requests"
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = "aws_instance.example.public_ip"
+  description = "The public ip address of the web server"
+}
